d5c338acd86e5e63c0bf7d5a7aececeb460a2310
dbs_jd <dbs_jd@172.20.2.38>
2015-10-07 07:01:07 -0600

    Primero

---
+++ Hoja9/src/Word.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author dbs_jd
+ */
+/*
+UVG
+Algoritmos y Estructuras de Datos - 2011
+Hoja de trabajo 7 
+Autor: Eduardo Castellanos
+
+Descripción: Word. Clase para almacenar las palabras junto con su tipo. 
+*/
+class Word implements Comparable<Word> {
+	private String word;
+	private String type;
+	
+	// Constructor, inicializa la palabra con su tipo
+	public Word(String word, String type)
+	{
+		this.word=word;
+		this.type=type;
+	}
+	
+	public Word()
+	{
+		this.word= "";
+		this.type="";
+	}
+	
+	// Comparadores, solo importa comparar la palabra, no el tipo.
+	public int compareTo(Word o)
+	{
+		return this.word.compareTo(o.getWord());
+	}
+	
+	public boolean equals(Object obj)
+	{
+		return (obj instanceof Word && getWord().equals(((Word)obj).getWord()));
+	}
+	
+	// Métodos de acceso..
+	public void setWord(String word)
+	{
+		this.word=word;
+	}
+	public void setType(String type)
+	{
+		this.type=type;
+	}
+	public String getWord()
+	{
+		return word;
+	}
+	
+	public String getType()
+	{
+		return type;
+	}
+	
+}
---
+++ Hoja9/src/WordSet.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author dbs_jd
+ */
+/*
+UVG
+Algoritmos y Estructuras de Datos - 2011
+Hoja de trabajo 7 
+Autor: Eduardo Castellanos
+
+Descripción: WordSet. Interfaz para las implementaciones de clases que almacenarán palabras.
+*/
+public interface WordSet {
+
+	public void add(Word wordObject);
+	public Word get(Word word);
+	
+}
